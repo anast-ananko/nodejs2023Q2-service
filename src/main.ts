52a23d4f86f323de6e6854653e3f6285211a6121
Nastassia Ananka <anast.ananko@gmail.com>
2023-07-28 23:36:15 +0200

    feat: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as jsYaml from 'js-yaml';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerFilePath = path.resolve(process.cwd(), 'doc', 'api.yaml');
+  const swaggerFileContent = fs.readFileSync(swaggerFilePath, 'utf8');
+  const swaggerObject = jsYaml.load(swaggerFileContent);
+  SwaggerModule.setup('/docs', app, swaggerObject);
 
   app.useGlobalPipes(new ValidationPipe());
 
