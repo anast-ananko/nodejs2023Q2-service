77ab22c9521fcffd0b672294a2b86625455e11dd
Nastassia Ananka <anast.ananko@gmail.com>
2023-08-12 17:58:02 +0200

    feat: add log file rotation

--- src/utils/writeData.ts
+++ src/utils/writeData.ts
@@ -1,8 +1,13 @@
 import * as path from 'path';
-import { writeFile, mkdir, appendFile } from 'fs/promises';
+import { writeFile, mkdir, appendFile, stat, rename } from 'fs/promises';
 import { existsSync } from 'fs';
 
 import { IMessage } from '../interfaces/message.interface';
+
+const MAX_LOG_FILE_SIZE = parseInt(
+  process.env.MAX_LOG_FILE_SIZE || '20000',
+  10,
+);
 
 const logDir = path.resolve(__dirname, '..', '..', 'logs');
 const logFilePath = path.resolve(logDir, 'app.log');
@@ -15,18 +20,35 @@ const dateFormat = new Intl.DateTimeFormat('en', {
   second: '2-digit',
 });
 
+const rotateLog = async () => {
+  const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+  const archiveFilePath = path.join(logDir, `app-${timestamp}.log`);
+
+  try {
+    await rename(logFilePath, archiveFilePath);
+  } catch (error) {
+    console.error('Log rotation error:', error);
+  }
+};
+
 export const writeData = async (level: string, message: IMessage) => {
   const timestamp = dateFormat.format(new Date());
   const logEntry = `[${timestamp}] [${level}] - ${JSON.stringify(message)}\n`;
 
-  if (!existsSync(logDir)) {
-    await mkdir(logDir);
-    await writeFile(logFilePath, '', { flag: 'wx' });
-  }
-
   try {
+    if (!existsSync(logDir)) {
+      await mkdir(logDir);
+      await writeFile(logFilePath, '', { flag: 'wx' });
+    }
+
+    const logFileSize = (await stat(logFilePath)).size;
+
+    if (logFileSize >= MAX_LOG_FILE_SIZE) {
+      await rotateLog();
+    }
+
     await appendFile(logFilePath, logEntry);
-  } catch (err) {
-    console.error('Error writing to log file:', err);
+  } catch (error) {
+    console.error('Error writing to log file:', error);
   }
 };
